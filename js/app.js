45cfa8894a533eebd216f4e71d166bff1acfa2e1
alim1919@alim191 <alimohammadian1919@gmail.com>
2022-02-26 23:43:41 +0330

    secound commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('.dropdown').click(function() {
+$(document).ready(function(){
+    $('.dropdown').click(function(){
         $(this).find('.dropdown-items').toggleClass('open');
     });
 });
